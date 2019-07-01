d29c46b59e0c97e06d95d1f6ed34cd2a6b12ad35
Isa Giongo <isagiongo@gmail.com>
2019-07-01 20:18:23 -0300

    Cria componente botao

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,70 +2,90 @@
   <div>
     <h1 class="titulo">{{ titulo }}</h1>
 
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Pesquise pelo título da foto">
+    <input
+      type="search"
+      class="filtro"
+      @input="filtro = $event.target.value"
+      placeholder="Pesquise pelo título da foto"
+    />
     {{ filtro }}
     <ul class="lista-fotos">
       <li class="lista-fotos-itens" v-for="foto of fotosComFiltro">
-
         <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao
+            rotulo="REMOVER"
+            tipo="button"
+            @botaoAtivado="remove(foto)"
+            :confirmacao="true"
+            estilo="alerta"
+          />
         </meu-painel>
-
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import Painel from '../shared/painel/Painel';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva';
+import Painel from "../shared/painel/Painel";
+import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva";
+import Botao from "../shared/botao/Botao";
 
 export default {
   components: {
-    'meu-painel' : Painel,
-    'imagem-responsiva' : ImagemResponsiva
+    "meu-painel": Painel,
+    "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao
   },
-  data () {
-    return {
-      titulo: 'Alurapic',
-      fotos: [],
-      filtro: ''
+
+  methods: {
+    remove(foto) {
+      alert("Remover foto: " + foto.titulo);
     }
+  },
+
+  data() {
+    return {
+      titulo: "Alurapic",
+      fotos: [],
+      filtro: ""
+    };
   },
   computed: {
     fotosComFiltro() {
       if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
+        let exp = new RegExp(this.filtro.trim(), "i");
         return this.fotos.filter(foto => exp.test(foto.titulo));
       } else {
         return this.fotos;
       }
     }
   },
-  created () {
-    this.$http.get('http://localhost:3000/v1/fotos')
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos, erro => console.log(erro));
+      .then(fotos => (this.fotos = fotos), erro => console.log(erro));
   }
-}
+};
 </script>
 
 <style>
-  .titulo {
-    text-align: center;
-  }
+.titulo {
+  text-align: center;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos .lista-fotos-itens {
-    display: inline-block;
-  }
+.lista-fotos .lista-fotos-itens {
+  display: inline-block;
+}
 
-  .filtro {
-    display: block;
-    width: 80%;
-    align-self: auto;
-  }
+.filtro {
+  display: block;
+  width: 80%;
+  align-self: auto;
+}
 </style>
---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,62 @@
+<template>
+  <button @click="disparaAcao()" :class="estiloDoBotao" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+export default {
+  
+  props: {
+      tipo: {
+          required: true,
+          type: String
+      },
+      rotulo: {
+          required: true,
+          type: String
+      },
+      confirmacao: Boolean,
+      estilo: String
+  },
+  
+  props: ["tipo", "rotulo", "confirmacao", "estilo"],
+
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm("Confirma operação?")) {
+          this.$emit("botaoAtivado");
+        }
+        return;
+      }
+      this.$emit("botaoAtivado");
+    }
+  },
+
+  computed: {
+    estiloDoBotao() {
+      if (this.estilo == "padrao" || !this.estilo) return "botao botao-padrao";
+      if (this.estilo == "alerta") return "botao botao-alerta";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.botao {
+  display: inline-block;
+  padding: 10px;
+  border-radius: 3px;
+  margin: 10px;
+  font-size: 1.2em;
+}
+
+.botao-alerta {
+  background: firebrick;
+  color: white;
+}
+
+.botao-padrao {
+  background: darkcyan;
+  color: white;
+}
+</style>
